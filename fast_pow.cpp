dbb130cd1025ead61ddf28342ba518ec472dc110
Edians-m <fortunate567@qq.com>
2026-05-03 13:04:26 +0800

    快速幂（取模）

---
+++ fast_pow.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+#define ll long long
+
+ll fastPow(ll a,ll b,ll mod){
+    ll res=1;
+    
+    while(b){
+        if(b & 1){
+            res=res * a % mod;
+        }
+
+        a=a * a % mod;
+        b>>=1;
+    }
+    return res;
+}
+
+int main(){
+    ll a, b, mod;
+    cin >> a >> b >> mod;
+    cout << fastPow(a, b, mod) << endl;
+}
