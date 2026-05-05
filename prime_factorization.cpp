866a3bf246a7d29bf20e59db8e2a6dc16d2a2748
Edians-m <fortunate567@qq.com>
2026-05-05 12:15:02 +0800

    质因数分解

---
+++ prime_factorization.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void factorize(n) {
+    for(int i = 2； i*i <= n; i++) {
+        if(n % i == 0) {
+            int cnt = 0;
+            while(n % i == 0) {
+                n /= i;
+                cnt++;
+            }
+
+            cout<<"i^"<<cnt;
+            if(n > 1) cout<<"*";
+        }
+    }
+
+    if(n > 1) cout<<n<<"^1"<<endl;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    factorize(n);
+}
