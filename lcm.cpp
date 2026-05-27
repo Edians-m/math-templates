141e9e40983fda9101daf56eea8efbdb3b9c58b2
Edians-m <fortunate567@qq.com>
2026-05-27 18:13:56 +0800

    Refactor gcd and lcm function parameter formatting

--- lcm.cpp
+++ lcm.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int gcd(int a,int b){
+int gcd(int a, int b){
     return b != 0 ? gcd(b,a%b) : a;   //可把 b != 0 改成 b
 }
 
-int lcm(int a,int b){
+int lcm(int a, int b){
     return a / gcd(a,b) * b;                 //先除是为了防溢出
 }
 
