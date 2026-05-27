ccc8d2ad75e62197a6948004cfd854df442fcf27
Edians-m <fortunate567@qq.com>
2026-05-27 18:12:08 +0800

    Fix formatting issues in sieve function

--- sieve_eratosthenes.cpp
+++ sieve_eratosthenes.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 vector<int> sieve(int n){
     vector<bool> isPrime(n+1,true);
-    isPrime[0]=isPrime[1]=false;
+    isPrime[0] = isPrime[1] = false;
 
-    for(int i=2; i*i<=n; ++i){
+    for(int i = 2; i*i <= n; ++i){
         if(isPrime[i])
-            for(int j=i*i; j<=n; j+=i)
-                isPrime[j]=false;
+            for(int j = i*i; j <= n; j += i)
+                isPrime[j] = false;
     }
 
     vector<int> primes;
-    for(int i=2; i<=n; ++i)
+    for(int i = 2; i <= n; ++i)
         if(isPrime[i])
             primes.push_back(i);
     return primes;
