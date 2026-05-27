94b7ea0015f7f8d5a661f2a8c426c5272d0dfbf9
Edians-m <fortunate567@qq.com>
2026-05-27 18:16:07 +0800

    Refactor fastPow function for readability

--- fast_pow.cpp
+++ fast_pow.cpp
@@ -2,21 +2,22 @@
 using namespace std;
 #define ll long long
 
-ll fastPow(ll a,ll b,ll mod){
-    ll res=1;
+ll fastPow(ll a, ll b, ll mod) {
+    ll res = 1;
     
-    while(b){
+    while(b) {
         if(b & 1){
-            res=res * a % mod;
+            res = res * a % mod;
         }
 
-        a=a * a % mod;
-        b>>=1;
+        a = a * a % mod;
+        b >>= 1;
     }
     return res;
 }
 
-int main(){
+int main()
+{
     ll a, b, mod;
     cin >> a >> b >> mod;
     cout << fastPow(a, b, mod) << endl;
