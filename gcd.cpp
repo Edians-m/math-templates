07e93c25e990ae2a1ce8bae808ffde2fb2c21363
Edians-m <fortunate567@qq.com>
2026-05-27 18:15:20 +0800

    Update gcd.cpp

--- gcd.cpp
+++ gcd.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int gcd1(int a,int b){
+int gcd1(int a, int b){
     while(b != 0){
-        int t=a%b;
-        a=b;
-        b=t;
+        int t = a % b;
+        a = b;
+        b = t;
     }
     return a;
 }
 
 //递归
-int gcd2(int a,int b){
+int gcd2(int a, int b){
     return b != 0 ? gcd2(b,a%b) : a;
 }
 
 int main(){
-    int a,b;
+    int a, b;
     cin >> a >> b;
     cout << gcd1(a, b) << endl;
     cout << gcd2(a, b) << endl;
